964bef5afdff73ee87eb270e7cf01a57020eb627
calm-collection-3916 <khalinredd@gmail.com>
2020-12-30 04:14:50 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+def using_concat(array_one, array_two)
+  array_one.concat(array_two)
+end 
+using_concat(["raindrops on roses", "whiskers on kittens"], ["sports cars", "flatiron school"])
+  
+  def using_insert(array, element)
+    array.insert(4,element)
+  end
+  using_insert(["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"], "Python")
+  
+  def using_uniq(array)
+    array.uniq
+  end
+  using_uniq(["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"])
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
