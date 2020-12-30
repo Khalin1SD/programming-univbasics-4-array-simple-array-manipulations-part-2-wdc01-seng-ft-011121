8f5711f10d860b4347cdf36ee45f7e68fc93f5e8
calm-collection-3916 <khalinredd@gmail.com>
2020-12-30 04:43:40 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,40 @@
+def using_concat(array_one, array_two)
+  array_one.concat(array_two)
+end 
+using_concat(["raindrops on roses", "whiskers on kittens"], ["sports cars", "flatiron school"])
+  
+  def using_insert(array, element)
+    array.insert(4,element)
+  end
+  using_insert(["Ruby", "JavaScript", "Java", "C#", "Objective C", "C++", "PHP"], "Python")
+  
+  def using_uniq(array)
+    array.uniq
+  end
+  using_uniq(["Pixie", "Bob", "Mohawk", "Crew Cut", "Linka", "Wheeler", "Bob"])
+  
+  def using_flatten(array)
+    array.flatten 
+  end
+  using_flatten(["Saxophone", ["Piano", "Trumpet"], "Violin", "Drums", "Flute"])
+  
+  def using_delete(array, string)
+    array.delete(string)
+  end
+  using_delete(["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"], "Steven")
+  
+  def using_delete_at(array, integer)
+    array.delete_at(integer)
+  end
+  using_delete_at(["Johnny 5", "R2D2", "Robocop"], 2)
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
